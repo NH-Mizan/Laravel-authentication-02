0b80e2eacfac15702f1d96a86f2b3443825d053f
Nahid Hasan Mizan <nhmizan999@gmail.com>
2025-06-16 12:24:41 +0600

    admin login data save and dashboard added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -9,3 +11,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['namespace' => 'Admin',  'prefix' => 'admin'], function () {
+    Route::get('dashboard', [DashboardController::class, 'dashboard'])->name('dashboard');
+  
+
+});
