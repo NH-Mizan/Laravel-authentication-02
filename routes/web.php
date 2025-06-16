5e6e2891684bf44c01bb0fef3276f9af00aec7c4
Nahid Hasan Mizan <nhmizan999@gmail.com>
2025-06-17 00:38:48 +0600

    general setting backend and frontend setup

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+use App\Http\Controllers\admin\GeneralSettingController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Admin\DashboardController;
@@ -36,6 +37,17 @@ Route::group(['namespace' => 'Admin',  'prefix' => 'admin', 'middleware' => ['au
     Route::post('users/inactive', [UserController::class, 'inactive'])->name('users.inactive');
     Route::post('users/active', [UserController::class, 'active'])->name('users.active');
     Route::post('users/destroy', [UserController::class, 'destroy'])->name('users.destroy');
+
+
+      // settings route
+    Route::get('settings/manage', [GeneralSettingController::class, 'index'])->name('settings.index');
+    Route::get('settings/create', [GeneralSettingController::class, 'create'])->name('settings.create');
+    Route::post('settings/save', [GeneralSettingController::class, 'store'])->name('settings.store');
+    Route::get('settings/{id}/edit', [GeneralSettingController::class, 'edit'])->name('settings.edit');
+    Route::post('settings/update', [GeneralSettingController::class, 'update'])->name('settings.update');
+    Route::post('settings/inactive', [GeneralSettingController::class, 'inactive'])->name('settings.inactive');
+    Route::post('settings/active', [GeneralSettingController::class, 'active'])->name('settings.active');
+    Route::post('settings/destroy', [GeneralSettingController::class, 'destroy'])->name('settings.destroy');
   
 
 });
